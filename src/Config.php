458ffae46b24bfd395ecb6780b7372aff2001b7b
Antoine Rubeo-lisa <73071779+Maison-hub@users.noreply.github.com>
2025-05-13 01:51:11 +0200

    :construction_worker: Add phpstan
    * fix all php stan errors
    
    * add php stan to ci
    
    * add composer.lock
    
    * install dependencies in ci

--- src/Config.php
+++ src/Config.php
@@ -4,6 +4,12 @@ namespace Maplee;
 
 class Config
 {
+    /**
+     * Load the configuration file.
+     *
+     * @param string|null $configPath Path to the configuration file.
+     * @return array<string, mixed> The loaded configuration.
+     */
     public static function load(?string $configPath = null): array
     {
         $defaultPath = __DIR__ . '/../config/maplee.php';
