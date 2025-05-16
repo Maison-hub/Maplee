dff9456173aaf90d21ca6a208cc661a4f09f9c81
Antoine Rubeo-lisa <73071779+Maison-hub@users.noreply.github.com>
2025-05-16 11:04:42 +0200

    :white_check_mark: add unit test
    * start to add unit test
    
    * undate php version in ci
    
    * try to run test in ci
    
    * downgrade version to php 8.2
    
    * fix phpstan errors
    
    * add phpunit.xml.dist file and RouterTest.php

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,8 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
+    'PhpParser\\' => array($vendorDir . '/nikic/php-parser/lib/PhpParser'),
+    'Maplee\\Tests\\' => array($baseDir . '/tests'),
     'Maplee\\' => array($baseDir . '/src'),
+    'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
 );
